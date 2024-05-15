3f9644410a2ad0f345b2ad079bdcbcca4702680e
Sidney Lima <sidneylimaux@gmail.com>
2024-05-15 09:54:30 -0300

    Fix: Menu style and links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import CalendarIcon from '../Images/Icons/calendar.svg';
 import Logo from '../Images/Logo Saude360.svg';
+import { IonIcon } from '@ionic/react';
+
+import CalendarIcon from '../Images/Icons/calendar.svg';
+import { menuOutline, closeOutline } from 'ionicons/icons';
 
 const SideMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +24,6 @@ const SideMenu = () => {
 
   return (
     <div className=" z-50">
-      {/* Overlay */}
       {isOpen && (
         <div
           className="fixed inset-0 bg-gray-600 opacity-50"
@@ -29,51 +31,53 @@ const SideMenu = () => {
         ></div>
       )}
 
-      {/* Side Menu */}
       <div
-        className={`transform-gpu transition-transform duration-300 bg-white w-64 h-full shadow-lg fixed top-0 left-0 overflow-y-auto ease-in-out ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        }`}
+        className={`transform-gpu transition-transform duration-300 bg-white w-64 h-full shadow-lg fixed top-0 left-0 overflow-y-auto ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'
+          }`}
       >
-        <div className="p-4">
+        <div className="p-4 h-90">
           <div className="flex flex-row-reverse">
             <button
-              className="text-gray-800 focus:outline-none focus:text-gray-500"
+              className=" focus:outline-none focus:text-gray-500 hover:bg-gray-50 text-gray-500 p-3 rounded-full "
               onClick={toggleMenu}
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+              <IonIcon icon={closeOutline} className="w-6 h-6 fill-gray2"></IonIcon>
             </button>
           </div>
-          <div className="mt-4">
-            <img src={Logo} alt="Logo Saude360" className="h- w-auto mx-auto" />
+          <div className='flex flex-col justify-between h-full'>
+            <div>
+              <div>
+                <a href="/home">
+                <img src={Logo} alt="Logo Saude360" className="h- w-auto mx-auto" />
+                </a>
+              </div>
+              <nav className="mt-8">
+                <ul>
+                  {routes.appPages.map((route, index) => (
+                    <li key={index}>
+                      <a
+                        href={route.path}
+                        className="flex items-center px-4 py-2 text-gray-800 bg-menu-color hover:rounded-md hover:text-white"
+                      >
+                        <IonIcon icon={route.icon} className="w-6 h-6 mr-2 fill-gray2"></IonIcon>
+                        {route.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <div>
+              <a
+                href=""
+                className="flex items-center px-4 py-2 text-gray-800 bg-menu-color hover:rounded-md hover:text-white"
+              >
+                <IonIcon icon={closeOutline} className="w-6 h-6 mr-2 fill-gray2"></IonIcon>
+                Sair
+              </a>
+            </div>
           </div>
-          <nav className="mt-8">
-            <ul>
-              {routes.appPages.map((route, index) => (
-                <li key={index}>
-                  <a
-                    href={route.path}
-                    className="flex items-center px-4 py-2 text-gray-800 hover:bg-blue-700 hover:text-color-white"
-                  >
-                    <img src={route.icon} alt="Ícone" className="w-6 h-6 mr-2" />
-                    {route.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+
         </div>
       </div>
 
@@ -83,19 +87,7 @@ const SideMenu = () => {
           className="fixed top-4 left-4 z-50 hover:bg-gray-50 text-color-blue1 p-3 rounded-full focus:outline-none"
           onClick={toggleMenu}
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <IonIcon icon={menuOutline} className="w-6 h-6 fill-gray2"></IonIcon>
         </button>
       )}
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,10 +11,7 @@ const Home: React.FC = () => {
         <Menu/>
         
         <div className="text-center absolute left-0 right-0 top-1/2 transform -translate-y-1/2 flex justify-center items-center">
-          <div className="border border-gray-100 rounded-xl shadow-2xl p-8 bg-gradient-to-r from-green-400 via-blue-900 to-blue-400 text-white max-w-lg">
-            <h1 className="text-2xl  font-bold">Using TailwindCSS in Ionic with React</h1>
-            <p className="mt-4">This is an example of how you can use <span className="font-bold">TailwindCSS</span> in an <span className="font-bold">Ionic</span> application using <span className="font-bold">React</span> framework.</p>
-          </div>
+          <h1>home</h1>
         </div>
       </IonContent>
     </IonPage>
