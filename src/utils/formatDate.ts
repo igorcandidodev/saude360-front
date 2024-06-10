5041e744b25f2b31c91fbe80a768508b17a51ad0
Sidney Lima <sidneylimadev@gmail.com>
2024-06-10 14:28:51 -0300

    Fix: Date and links

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,5 @@
-export function formatDate(dateString: string): string {
+export function formatDate(dateString: string | null | undefined): string {
+  if (!dateString) return ""; // Verifica se o dateString é nulo ou indefinido
   const [year, month, day] = dateString.split("-");
   return `${day}/${month}/${year}`;
 }
--- src/utils/formatDateAndTime.ts
+++ src/utils/formatDateAndTime.ts
@@ -1,4 +1,5 @@
-export function formatDateTime(dateTimeString: string): string {
+export function formatDateTime(dateTimeString: string | null | undefined): string {
+    if (!dateTimeString) return ""; // Verifica se o dateTimeString é nulo ou indefinido
     const [date] = dateTimeString.split('T'); // Separa a data da hora
     const [year, month, day] = date.split('-'); // Separa o ano, mês e dia
     return `${day}/${month}/${year}`; // Retorna a data formatada
