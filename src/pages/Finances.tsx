ea4d8953566b779ea224423415c26701caca09eb
Sidney Lima <sidneylimadev@gmail.com>
2024-11-11 16:42:51 -0300

    fix: style web and mobile

--- src/pages/Finances.tsx
+++ src/pages/Finances.tsx
@@ -4,56 +4,53 @@ import {
   IonModal,
   IonPage,
   IonTitle,
-  IonToolbar
-} from '@ionic/react';
-import { Link } from 'react-router-dom';
-import Menu from '../components/Menu';
+  IonToolbar,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
-import React, { useEffect, useRef, useState } from 'react';
-import FinanceBalance from '../components/FinanceBalance';
-import { FinanceTable } from '../components/FinanceTable';
-import { Form } from '../components/FormRegister';
-import FormActionButton from '../components/FormRegister/FormActionButton';
-import TransactionService from '../core/services/TransactionService';
-import ActionButton from '../components/ButtonComponent/ActionButton';
-
-
+import React, { useEffect, useRef, useState } from "react";
+import FinanceBalance from "../components/FinanceBalance";
+import { FinanceTable } from "../components/FinanceTable";
+import { Form } from "../components/FormRegister";
+import FormActionButton from "../components/FormRegister/FormActionButton";
+import TransactionService from "../core/services/TransactionService";
+import ActionButton from "../components/ButtonComponent/ActionButton";
 
 const Finances: React.FC = () => {
   const [transactions, setTransactions] = useState([]);
-  const formRef = useRef<any>(null); 
+  const formRef = useRef<any>(null);
 
-  const [showModal, setShowModal] = useState(false); 
+  const [showModal, setShowModal] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    value: '',
-    transactionType: 'Entrada',
-    date: '',
-    paymentMethod: 'Pix',
-    paymentStatus: 'Pendente'
+    name: "",
+    value: "",
+    transactionType: "Entrada",
+    date: "",
+    paymentMethod: "Pix",
+    paymentStatus: "Pendente",
   });
-  const [selectedTransaction, setSelectedTransaction] = useState(null); 
+  const [selectedTransaction, setSelectedTransaction] = useState(null);
 
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   const transactionService = new TransactionService();
 
-
   const handleEditTransaction = (transaction) => {
     setSelectedTransaction(transaction);
-    setFormData(transaction); 
+    setFormData(transaction);
     setShowModal(true);
   };
 
   const fetchTransactions = async () => {
     try {
-      setLoading(true); 
+      setLoading(true);
       const data = await transactionService.getAllTransactions();
-      setTransactions(data); 
+      setTransactions(data);
     } catch (error) {
-      console.error('Erro ao obter transações:', error);
+      console.error("Erro ao obter transações:", error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -61,30 +58,30 @@ const Finances: React.FC = () => {
     fetchTransactions();
   }, []);
 
-   const handleInputChange = (e: any) => {
+  const handleInputChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name]: value
+      [name]: value,
     }));
   };
-
-
 
   const handleAddTransaction = async (formData: any) => {
     try {
       if (selectedTransaction) {
-        await transactionService.updateTransaction(formData, selectedTransaction.id); 
+        await transactionService.updateTransaction(
+          formData,
+          selectedTransaction.id
+        );
       } else {
-        await transactionService.createTransaction(formData); 
+        await transactionService.createTransaction(formData);
       }
-      fetchTransactions(); 
+      fetchTransactions();
       setShowModal(false);
     } catch (error) {
-      console.error('Erro ao adicionar transação:', error);
+      console.error("Erro ao adicionar transação:", error);
     }
   };
-
 
   const handleSubmit = () => {
     if (formRef.current) {
@@ -93,58 +90,69 @@ const Finances: React.FC = () => {
   };
 
   return (
-    <IonPage className='justify-start px-5 bg-gray3'>
+    <IonPage className="justify-start px-5 bg-gray3">
       <Menu />
-      <div className='h-screen flex items-start mt-20 lg:mt-10 '>
-        
-        <div className=' flex flex-col w-full lg:justify-center'>
-          <div className='flex justify-center '>
-            <div className='flex-col  lg:w-3/4 '>
-              <div className='flex justify-between mx-5 lg:mx-0 '>
-              <h1 className='text-3xl'>Financeiro</h1>
-              <ActionButton
-                text="ADICIONAR NOVO"
-                onClick={() => setShowModal(true)}
+      <div className="h-screen flex items-start mt-20 lg:mt-10 ">
+        <div className=" flex flex-col w-full lg:justify-center">
+          <div className="flex justify-center ">
+            <div className="flex-col  lg:w-3/4 ">
+              <div className="flex justify-between flex-col md:w-full md:items-end md:flex-row w-11/12 mx-5 lg:mx-0 ">
+                <h1 className="text-3xl">Financeiro</h1>
+                <ActionButton
+                  text="ADICIONAR NOVO"
+                  onClick={() => setShowModal(true)}
                 />
 
                 {/* <IonButton onClick={() => setShowModal(true)} className="bg-blue1 text-white px-4 rounded-md w-48 my-1">
                   Adicionar novo
                 </IonButton> */}
               </div>
-              <div className='flex justify-center'>
-              <FinanceBalance transactions={transactions} loading={loading} />
+              <div className="flex justify-center">
+                <FinanceBalance transactions={transactions} loading={loading} />
               </div>
-
             </div>
-
-
           </div>
-          <div className='flex justify-center'>
-            <div className='w-full lg:w-3/4'>
-              <FinanceTable transactions={transactions} loading={loading} onEdit={handleEditTransaction} />
+          <div className="flex justify-center">
+            <div className="w-full lg:w-3/4">
+              <FinanceTable
+                transactions={transactions}
+                loading={loading}
+                onEdit={handleEditTransaction}
+              />
             </div>
-
           </div>
         </div>
       </div>
-      
 
-      <IonModal isOpen={showModal} onDidDismiss={() => { setShowModal(false); setSelectedTransaction(null); }}> {/* Resetar no fechamento */}
+      <IonModal
+        isOpen={showModal}
+        onDidDismiss={() => {
+          setShowModal(false);
+          setSelectedTransaction(null);
+        }}
+      >
+        {" "}
+        {/* Resetar no fechamento */}
         <IonHeader>
           <IonToolbar>
-            <IonTitle>{selectedTransaction ? 'Editar Transação' : 'Nova Transação'}</IonTitle>
-
+            <IonTitle>
+              {selectedTransaction ? "Editar Transação" : "Nova Transação"}
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
           <div className="flex flex-col justify-center items-center p-8">
-            <Form.Finance ref={formRef} onSubmit={handleAddTransaction} formData={selectedTransaction} />
+            <Form.Finance
+              ref={formRef}
+              onSubmit={handleAddTransaction}
+              formData={selectedTransaction}
+            />
           </div>
           <div className="flex justify-center">
             <FormActionButton
               text="SALVAR"
               type="submit"
-              onClick={handleSubmit} 
+              onClick={handleSubmit}
             />
           </div>
         </IonContent>
--- src/pages/Patients.tsx
+++ src/pages/Patients.tsx
@@ -1,43 +1,62 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonRow, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
-import Menu from '../components/Menu';
-import FilterButton from '../components/FilterButton';
-import PatientsTable from '../components/PatientsTable';
-import { PatientsTableProvider } from '../context/PatientsTableContext';
-import { useHistory } from 'react-router-dom';
-import ActionButton from '../components/ButtonComponent/ActionButton';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonRow,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import Menu from "../components/Menu";
+import FilterButton from "../components/FilterButton";
+import PatientsTable from "../components/PatientsTable";
+import { PatientsTableProvider } from "../context/PatientsTableContext";
+import { useHistory } from "react-router-dom";
+import ActionButton from "../components/ButtonComponent/ActionButton";
 
 const Patients: React.FC = () => {
-
   const history = useHistory();
 
   const addNewPatient = () => {
-    history.push('/cadastro-paciente');
+    history.push("/cadastro-paciente");
   };
 
   return (
-    <PatientsTableProvider> {/* Envolvendo toda a página com o Provider */}
-      <IonPage className='justify-start'>
+    <PatientsTableProvider>
+      {" "}
+      {/* Envolvendo toda a página com o Provider */}
+      <IonPage className="justify-start">
         <Menu />
-        <div className='h-full flex items-start mt-10'>
-          <div className='flex flex-col lg:justify-center'>
-            <div className='flex justify-center'>
-              <div className='flex-col w-full mx-5 lg:w-3/4 mt-10 lg:mt-0'>
-                <h1 className='text-3xl'>Pacientes</h1>
-                <div className='flex justify-between'>
-                  <div className='flex w-2/5 lg:w-full items-center'>
-                    <IonSearchbar animated={true} placeholder="Pesquise o nome do paciente" className='w-[22rem]'></IonSearchbar>
-                    <FilterButton /> {/* Este componente agora tem acesso ao contexto */}
+        <div className="h-full flex items-start mt-10 w-screen">
+          <div className="flex flex-col lg:justify-center">
+            <div className="flex justify-center">
+              <div className="flex-col  mx-5 lg:w-3/4 mt-10 lg:mt-0">
+                <h1 className="text-3xl">Pacientes</h1>
+                <div className="flex justify-between flex-col md:flex-row md:w-full w-fit md:gap-20">
+                  <div className="flex w-fit w-md:w-2/5 lg:w-full items-end mb-1">
+                    <IonSearchbar
+                      animated={true}
+                      placeholder="Nome do paciente"
+                      className="w-full p-0 px-2"
+                    ></IonSearchbar>
+                    <FilterButton />{" "}
+                    {/* Este componente agora tem acesso ao contexto */}
                   </div>
-                  <ActionButton
-                    text="ADICIONAR NOVO PACIENTE"
-                    onClick={addNewPatient}
-                  />
+                  <ActionButton text="ADICIONAR NOVO" onClick={addNewPatient} />
                 </div>
               </div>
             </div>
-            <div className='flex w-screen overflow-scroll p-5 justify-center'>
-              <div className='w-full lg:mx-0 lg:w-3/4 '>
-                <PatientsTable /> {/* Este componente também tem acesso ao contexto */}
+            <div className="flex w-screen overflow-scroll p-5 justify-center">
+              <div className="w-full lg:mx-0 lg:w-3/4 ">
+                <PatientsTable />{" "}
+                {/* Este componente também tem acesso ao contexto */}
               </div>
             </div>
           </div>
